c027ede833cc51ee1bc0b3f6adabf06397edf065
Eugeny <bratchkov1999@gmail.com>
2023-03-27 20:03:38 +0700

    Revert "Написан класс Child."
    This reverts commit 2ca65b44d620aa6b1016fdb63a4d83cda8464751.

--- LAB2/Model/Child.cs
+++ LAB2/Model/Child.cs
@@ -1,95 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Model
 {
-    /// <summary>
-    /// Ребенок, наследник PersonBase.
-    /// </summary>
     public class Child : PersonBase
     {
         /// <summary>
-        /// Gets минимальный возраст.
+        /// Минимальный возраст
         /// </summary>
         public override int MinAge => 0;
 
         /// <summary>
-        /// Gets максимальный возраст.
+        /// Максимальный возраст
         /// </summary>
         public override int MaxAge => 17;
 
-        /// <summary>
-        /// Колония по пробыванию мозгов.
-        /// </summary>
-        private string? _institution;
-
-        /// <summary>
-        /// Gets or sets задание матери.
-        /// </summary>
-        public Adult? Mother { get; set; }
-
-        /// <summary>
-        /// Gets or sets задание отца.
-        /// </summary>
-        public Adult? Father { get; set; }
-
-        /// <summary>
-        /// Gets or sets задание детсада/школы.
-        /// </summary>
-        public string Institution
-        {
-            get
-            {
-                return _institution;
-            }
-
-            set
-            {
-                _institution = value;
-            }
-        }
-
-        /// <summary>
-        /// Проверка есть ли родитель.
-        /// </summary>
-        /// <param name="perent">Родитель.</param>
-        /// <param name="name">Имя.</param>
-        /// <returns>Информационная строка о наличии родителя.</returns>
-        private static string CheckParents(Adult perent,
-                                            string name = "Мама")
-        {
-            if (perent != null)
-            {
-                return $"\n{name}: {perent.Name} {perent.Surname}, ";
-            }
-
-            else
-            {
-                return $"\nСочувствую, но у тебя отсутствует {name}. ";
-            }
-        }
-
-        /// <summary>
-        /// Метод получения информации.
-        /// </summary>
-        /// <returns>Информация о ребёнке.</returns>
-        public override string GetInfo()
-        {
-            string personInfo = base.GetInfo();
-
-            personInfo += CheckParents(Mother, "Мама");
-            personInfo += CheckParents(Father, "Отец");
-
-            personInfo += "\nМесто промывки мозгов ";
-            if (string.IsNullOrEmpty(Institution))
-            {
-                personInfo += "Этот ребенок не встраивается в систему " +
-                    "воспроизводства производственных отношений! Он должен " +
-                    "быть институализирован.";
-            }
-            else
-            {
-                personInfo += Institution;
-            }
-
-            return personInfo;
-        }
     }
 }
