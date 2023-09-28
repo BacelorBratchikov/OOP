e528ad1efdb23ccfc46b2e9e3b4ddd81994e2c2d
Евгений Братчиков <bratchkov1999@gmail.com>
2023-09-28 10:39:39 +0700

    Создан фильтр

--- LAB4/WindowsFormsLab4/WinFormsApp/FilterForm.Designer.cs
+++ LAB4/WindowsFormsLab4/WinFormsApp/FilterForm.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FilterForm));
+            groupBox1 = new GroupBox();
+            CaloriiCheckBox = new CheckBox();
+            SwimmingCheckBox = new CheckBox();
+            RunningCheckBox = new CheckBox();
+            label1_cal = new Label();
+            CaloriiTextBox = new TextBox();
+            BarbellPressCheckBox = new CheckBox();
+            FilterButton = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(CaloriiCheckBox);
+            groupBox1.Controls.Add(SwimmingCheckBox);
+            groupBox1.Controls.Add(RunningCheckBox);
+            groupBox1.Controls.Add(label1_cal);
+            groupBox1.Controls.Add(CaloriiTextBox);
+            groupBox1.Controls.Add(BarbellPressCheckBox);
+            groupBox1.Location = new Point(20, 20);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(226, 149);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Параметры фильтрации";
+            // 
+            // CaloriiCheckBox
+            // 
+            CaloriiCheckBox.AutoSize = true;
+            CaloriiCheckBox.Location = new Point(10, 118);
+            CaloriiCheckBox.Name = "CaloriiCheckBox";
+            CaloriiCheckBox.Size = new Size(92, 24);
+            CaloriiCheckBox.TabIndex = 6;
+            CaloriiCheckBox.Text = "Калории";
+            CaloriiCheckBox.UseVisualStyleBackColor = true;
+            CaloriiCheckBox.CheckedChanged += checkBoxCalorii_CheckedChanged;
+            // 
+            // SwimmingCheckBox
+            // 
+            SwimmingCheckBox.AutoSize = true;
+            SwimmingCheckBox.Location = new Point(10, 87);
+            SwimmingCheckBox.Name = "SwimmingCheckBox";
+            SwimmingCheckBox.Size = new Size(100, 24);
+            SwimmingCheckBox.TabIndex = 5;
+            SwimmingCheckBox.Text = "Плавание";
+            SwimmingCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // RunningCheckBox
+            // 
+            RunningCheckBox.AutoSize = true;
+            RunningCheckBox.Location = new Point(10, 56);
+            RunningCheckBox.Name = "RunningCheckBox";
+            RunningCheckBox.Size = new Size(54, 24);
+            RunningCheckBox.TabIndex = 4;
+            RunningCheckBox.Text = "Бег";
+            RunningCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1_cal
+            // 
+            label1_cal.AutoSize = true;
+            label1_cal.Location = new Point(181, 118);
+            label1_cal.Name = "label1_cal";
+            label1_cal.Size = new Size(39, 20);
+            label1_cal.TabIndex = 3;
+            label1_cal.Text = "ккал";
+            // 
+            // CaloriiTextBox
+            // 
+            CaloriiTextBox.Location = new Point(112, 115);
+            CaloriiTextBox.Name = "CaloriiTextBox";
+            CaloriiTextBox.Size = new Size(63, 27);
+            CaloriiTextBox.TabIndex = 2;
+            CaloriiTextBox.TextChanged += textBoxCalorii_TextChanged;
+            CaloriiTextBox.KeyPress += textBoxCalorii_KeyPress;
+            // 
+            // BarbellPressCheckBox
+            // 
+            BarbellPressCheckBox.AutoSize = true;
+            BarbellPressCheckBox.Location = new Point(10, 26);
+            BarbellPressCheckBox.Name = "BarbellPressCheckBox";
+            BarbellPressCheckBox.Size = new Size(118, 24);
+            BarbellPressCheckBox.TabIndex = 1;
+            BarbellPressCheckBox.Text = "Жим штанги";
+            BarbellPressCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // FilterButton
+            // 
+            FilterButton.Location = new Point(30, 174);
+            FilterButton.Name = "FilterButton";
+            FilterButton.Size = new Size(200, 30);
+            FilterButton.TabIndex = 1;
+            FilterButton.Text = "Запуск фильтра";
+            FilterButton.UseVisualStyleBackColor = true;
+            FilterButton.Click += buttonFilter_Click;
+            // 
+            // FilterForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(271, 216);
+            Controls.Add(FilterButton);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "FilterForm";
+            Text = "Фильтр";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label label1_cal;
+        private TextBox CaloriiTextBox;
+        private CheckBox BarbellPressCheckBox;
+        private Button FilterButton;
+        private CheckBox CaloriiCheckBox;
+        private CheckBox SwimmingCheckBox;
+        private CheckBox RunningCheckBox;
     }
 }
--- LAB4/WindowsFormsLab4/WinFormsApp/FilterForm.cs
+++ LAB4/WindowsFormsLab4/WinFormsApp/FilterForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model.BaseAbstractClass;
+using Model.Exercises;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,163 @@ namespace WinFormsApp
 {
     public partial class FilterForm : Form
     {
-        public FilterForm()
+        /// <summary>
+        /// Лист фильтруемых фигур
+        /// </summary>
+        private readonly BindingList<BaseExerсise> _listExerсise;
+
+        /// <summary>
+        /// Лист отфильтрованных фигур
+        /// </summary>
+        private BindingList<BaseExerсise> _listExerсiseFilter;
+
+
+        /// <summary>
+        /// Обработчик события.
+        /// </summary>
+        public EventHandler<EventArgs> ExerсiseFiltered;
+
+        /// <summary>
+        /// Калории.
+        /// </summary>
+        private double calorii;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilterForm"/> class.
+        /// Форма фильтрации.
+        /// </summary>
+        public FilterForm(BindingList<BaseExerсise> exerсise)
         {
             InitializeComponent();
+            _listExerсise = exerсise;
+            CaloriiTextBox.Enabled = false;
         }
+
+        /// <summary>
+        /// Ввод калориев.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxCalorii_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (CaloriiTextBox.Text != string.Empty)
+                {
+                    calorii = Utils.CheckNumber(CaloriiTextBox.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректное число!",
+                    "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Контроль ввода значений.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxCalorii_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Utils.CheckInput(e);
+        }
+
+        /// <summary>
+        /// Флажок активации поля ввода объёма.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkBoxCalorii_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CaloriiCheckBox.Checked)
+            {
+                CaloriiTextBox.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Кнопка поиска.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonFilter_Click(object sender, EventArgs e)
+        {
+            _listExerсiseFilter = new BindingList<BaseExerсise>();
+
+            int count = 0;
+            if (!BarbellPressCheckBox.Checked
+                && !RunningCheckBox.Checked
+                && !SwimmingCheckBox.Checked
+                && !CaloriiCheckBox.Checked)
+            {
+                MessageBox.Show("Критерии для поиска не введены!",
+                    "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (BaseExerсise exerсise in _listExerсise)
+            {
+                switch (exerсise)
+                {
+                    case BarbellPress when BarbellPressCheckBox.Checked:
+                    case Running when RunningCheckBox.Checked:
+                    case Swimming when SwimmingCheckBox.Checked:
+                        {
+                            if (CaloriiCheckBox.Checked)
+                            {
+                                if (exerсise.Calories == calorii)
+                                {
+                                    count++;
+                                    _listExerсiseFilter.Add(exerсise);
+                                    break;
+                                }
+
+                                break;
+                            }
+                            else
+                            {
+                                count++;
+                                _listExerсiseFilter.Add(exerсise);
+                                break;
+                            }
+                        }
+                }
+
+                if (!BarbellPressCheckBox.Checked
+                    && !RunningCheckBox.Checked
+                    && !SwimmingCheckBox.Checked)
+                {
+                    if (CaloriiCheckBox.Checked && exerсise.Calories == calorii)
+                    {
+                        count++;
+                        _listExerсiseFilter.Add(exerсise);
+                    }
+                }
+            }
+
+            ExerciseListEventArgs eventArgs;
+
+            if (count > 0)
+            {
+                eventArgs = new ExerciseListEventArgs(_listExerсiseFilter);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Нет упражнений удовлетворяющих фильтру!",
+                    "Внимание!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                eventArgs = new ExerciseListEventArgs(_listExerсiseFilter);
+                return;
+            }
+
+            ExerсiseFiltered?.Invoke(this, eventArgs);
+            Close();
+        }
+
     }
 }
