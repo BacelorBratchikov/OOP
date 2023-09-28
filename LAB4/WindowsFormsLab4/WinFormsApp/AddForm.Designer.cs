c825b7f5d03ec1fc43b8a9288c98bd26b10de571
Евгений Братчиков <bratchkov1999@gmail.com>
2023-09-28 11:15:26 +0700

    AddForm

--- LAB4/WindowsFormsLab4/WinFormsApp/AddForm.Designer.cs
+++ LAB4/WindowsFormsLab4/WinFormsApp/AddForm.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddForm";
+            Random = new Button();
+            Cancel = new Button();
+            OK = new Button();
+            comboBoxExercise = new ComboBox();
+            label_Chouse = new Label();
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // Random
+            // 
+            Random.Location = new Point(209, 52);
+            Random.Name = "Random";
+            Random.Size = new Size(140, 30);
+            Random.TabIndex = 26;
+            Random.Text = "Случайная фигура";
+            Random.UseVisualStyleBackColor = true;
+            Random.Click += buttonRandom_Click;
+            // 
+            // Cancel
+            // 
+            Cancel.Location = new Point(209, 280);
+            Cancel.Name = "Cancel";
+            Cancel.Size = new Size(140, 30);
+            Cancel.TabIndex = 25;
+            Cancel.Text = "Отменить";
+            Cancel.UseVisualStyleBackColor = true;
+            Cancel.Click += buttonCancel_Click;
+            // 
+            // OK
+            // 
+            OK.Location = new Point(20, 280);
+            OK.Name = "OK";
+            OK.Size = new Size(140, 30);
+            OK.TabIndex = 23;
+            OK.Text = "Ok";
+            OK.UseVisualStyleBackColor = true;
+            OK.Click += buttonOK_Click;
+            // 
+            // comboBoxExercise
+            // 
+            comboBoxExercise.FormattingEnabled = true;
+            comboBoxExercise.Location = new Point(20, 52);
+            comboBoxExercise.Name = "comboBoxExercise";
+            comboBoxExercise.Size = new Size(168, 28);
+            comboBoxExercise.TabIndex = 22;
+            comboBoxExercise.SelectedIndexChanged += comboBoxExercise_SelectedIndexChanged;
+            // 
+            // label_Chouse
+            // 
+            label_Chouse.AutoSize = true;
+            label_Chouse.Location = new Point(20, 20);
+            label_Chouse.Name = "label_Chouse";
+            label_Chouse.Size = new Size(168, 20);
+            label_Chouse.TabIndex = 21;
+            label_Chouse.Text = "Выберете упражнение";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(20, 100);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(329, 170);
+            groupBox1.TabIndex = 30;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Параметры упражнений";
+            // 
+            // AddForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(361, 325);
+            Controls.Add(Random);
+            Controls.Add(Cancel);
+            Controls.Add(OK);
+            Controls.Add(comboBoxExercise);
+            Controls.Add(label_Chouse);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "AddForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Добавление упражнения";
+            Load += AddForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Random;
+        private Button Cancel;
+        private Button OK;
+        private ComboBox comboBoxExercise;
+        private Label label_Chouse;
+        private AddBarbellPressUserControl addBarbellPressUserControl1;
+        private AddRunningUserControl addRunningUserControl1;
+        private AddSwimmingUserControl addSwimmingUserControl1;
+        private GroupBox groupBox1;
     }
 }
--- LAB4/WindowsFormsLab4/WinFormsApp/AddForm.cs
+++ LAB4/WindowsFormsLab4/WinFormsApp/AddForm.cs
@@ -7,14 +7,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinFormsApp.AddExercises.Interface;
 
 namespace WinFormsApp
 {
-    public partial class AddForm : Form
+    public partial class AddForm : Form, IAddedable
     {
+        /// <summary>
+        /// Событие добавления фигуры.
+        /// </summary>
+        public EventHandler<EventArgs> ExerciseAdded;
+
+        /// <summary>
+        /// Словарь UserControls
+        /// </summary>
+        private readonly Dictionary<string, UserControl>
+            _comboBoxToUserControl;
+
+        /// <summary>
+        /// Метка используемого UserControl.
+        /// </summary>
+        private UserControl userControl;
+
         public AddForm()
         {
             InitializeComponent();
+
+            comboBoxExercise.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            OK.Enabled = false;
+
+            string[] typeExercise = { "Жим штанги", "Плавание", "Бег" };
+
+            comboBoxExercise.Items.AddRange(new string[]
+                 { typeExercise[0], typeExercise[1], typeExercise[2] });
+
+            _comboBoxToUserControl = new Dictionary<string, UserControl>()
+            {
+                {typeExercise[0], addBarbellPressUserControl1},
+                {typeExercise[1], addSwimmingUserControl1},
+                {typeExercise[2], addRunningUserControl1}
+            };
+        }
+
+        /// <summary>
+        /// Метод загрузки формы.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddForm_Load(object sender, EventArgs e)
+        {
+            addBarbellPressUserControl1.Visible = false;
+            addRunningUserControl1.Visible = false;
+            addSwimmingUserControl1.Visible = false;
+        }
+
+        /// <summary>
+        /// Выпадающий список.
+        /// Действие которое выполняется когда
+        /// выбрали фигуру из выпадающего списка.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBoxExercise_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string figureType = comboBoxExercise.SelectedItem.ToString();
+            foreach (var (figure, userControl) in _comboBoxToUserControl)
+            {
+                userControl.Visible = false;
+                if (figureType == figure)
+                {
+                    userControl.Visible = true;
+                    OK.Enabled = true;
+                    this.userControl = userControl;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Применить.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var currentExerciseControlName = comboBoxExercise.SelectedItem.ToString();
+                var currentExerciseControl = _comboBoxToUserControl[currentExerciseControlName];
+                var eventArgs = new ExerciseEventArgs(((IAddedable)currentExerciseControl).AddExercise());
+                ExerciseAdded?.Invoke(this, eventArgs);
+                DialogResult = DialogResult.OK;
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Введено некорректное значение!\n" +
+                    "Введите одно положительное число" +
+                    " в каждое текстовое поле.",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Закрыть.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Рандом.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            comboBoxExercise.SelectedIndex = random.Next(0, 3);
+
+            foreach (TextBox textbox in userControl.Controls.OfType<TextBox>())
+            {
+                if (textbox.Visible && String.IsNullOrEmpty(textbox.Text))
+                {
+                    textbox.Text = random.Next(1, 100).ToString();
+                }
+            }
         }
     }
 }
